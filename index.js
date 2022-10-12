3db501d98ac5ec344a6c0cdca1aa8c644300155f
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-10-11 23:26:44 -0600

    inquirer logic created for main menu. links to the view items. View items ask if the user would like to return or exit after displaying table.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
       // MySQL username,
       user: 'root',
       // MySQL password
-      password: '',
+      password: 'bob ross paint night',
       database: 'employee_db'
     },
     console.log(`Connected to the employee_db database.`)
@@ -20,11 +20,131 @@ const db = mysql.createConnection(
       type: 'list',
       message: 'What would you like to do?',
       name: 'mainMenu',
-      choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Departmnet", "Add A Roll", "Add An Employee","Update An Employee's Role" ],
+      choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Departmnet", "Add A Roll", "Add An Employee","Update An Employee's Role","Exit" ],
     }
   ])
   .then((menuChoice) => {
-    console.log (menuChoice)
+    
+    userInput = menuChoice.mainMenu
+    console.log (userInput)
+    if (userInput == "View All Departments"){
+        viewDepartments()
+    }
+    else if (userInput == "View All Roles") {
+        viewRoles()
+    }
+    else if (userInput == "View All Employees") {
+        viewEmployees()
+    }
+    else if (userInput == "Add A Departmnet") {
+        addDepartment()
+    }
+    else if (userInput == "Add A Roll") {
+        addRoll()
+    }
+    else if (userInput == "Add An Employee") {
+        addEmployee()
+    }
+    else if (userInput == "Update An Employee's Role") {
+        updateEmployee()
+    }
+    else if (userInput == "Exit") {
+        exit()
+    }
+    else {
+        console.log ("no input")
+    }
+})
+}
 
-  })
-  }
+    var viewDepartments = () => {
+        console.log("I Made It Here")
+
+        inquirer
+        .prompt([
+          {
+            type: 'list',
+            message: 'Would you like to return to the menu or exit?',
+            name: 'rtn',
+            choices: ["Return", "Exit"],
+          }
+        ])
+        .then((rtn) => {
+            userInput = rtn.rtn
+            if (userInput == "Return") {
+                mainMenu()
+            }
+            else {
+                exit()
+            }
+        })
+    }
+
+    var viewRoles = () => {
+        console.log("I Made It Here")
+
+        inquirer
+        .prompt([
+          {
+            type: 'list',
+            message: 'Would you like to return to the menu or exit?',
+            name: 'rtn',
+            choices: ["Return", "Exit"],
+          }
+        ])
+        .then((rtn) => {
+            userInput = rtn.rtn
+            if (userInput == "Return") {
+                mainMenu()
+            }
+            else {
+                exit()
+            }
+        })
+    }
+
+    var viewEmployees = () => {
+        console.log("I Made It Here")
+
+        inquirer
+        .prompt([
+          {
+            type: 'list',
+            message: 'Would you like to return to the menu or exit?',
+            name: 'rtn',
+            choices: ["Return", "Exit"],
+          }
+        ])
+        .then((rtn) => {
+            userInput = rtn.rtn
+            if (userInput == "Return") {
+                mainMenu()
+            }
+            else {
+                exit()
+            }
+        })
+    }
+
+    var addDepartment = () => {
+        console.log("I Made It Here")
+    }
+
+    var addRoll = () => {
+        console.log("I Made It Here")
+    }
+
+    var addEmployee = () => {
+        console.log("I Made It Here")
+    }
+
+    var updateEmployee = () => {
+        console.log("I Made It Here")
+    }
+
+    var exit = () => {
+        console.log("I Made It Here")
+    }
+
+
+  mainMenu()
