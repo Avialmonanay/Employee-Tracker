39afba22e724100f44f9dff510647020759ad868
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-10-11 22:23:13 -0600

    file structure built, schema design complete.

---
+++ schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  manager VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+role_name VARCHAR(50) NOT NULL,
+role_salary INT NOT NULL,
+department VARCHAR(50) NOT NULL,
+)
+
+CREATE TABLE departments(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(50) NOT NULL,
+)
